c8d9576929b5951e498962a5bf0ecefa2a0d8bbf
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-07-28 11:40:46 +0200

    Back to development: 3.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = "\n\n".join(
 
 setup(
     name="cs.srcset",
-    version="3.0.0",
+    version="3.0.1.dev0",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
